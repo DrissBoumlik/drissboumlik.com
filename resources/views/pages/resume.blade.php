d9e8b87cebdb14a89b04ac6257a88fd6560a1cc1
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-30 00:10:30 +0100

    Add exp section

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -7,23 +7,13 @@
         @include('pages.partials.about')
         {{-- @include('pages.index-parts.activities') --}}
         <div class="sections">
-            {{-- @foreach ($data->sections as $key => $section)
+            @foreach ($data->sections as $key => $section)
             @php $i = array_search($key,array_keys($data->sections)) @endphp
-            <div class="{{ $key }} section {{ $i%2 == 0 ? 'tc-grey-light-bg' : 'tc-white-bg' }}" style="height: 500px">
+            <div class="{{ $key }} section {{ $i%2 == 0 ? 'tc-grey-light-bg' : 'tc-white-bg' }}">
                 @include('pages.sections.'.$key, [$key => $section])
             </div>
-            @endforeach --}}
-            @include('pages.sections.competences', ['competences' => $data->sections['competences']])
+            @endforeach
         </div>
-            {{-- @include('pages.sections.competences', ['competences' => $data->competences])
-            @include('pages.sections.experiences', ['experiences' => $data->experiences])
-            @include('pages.sections.education', ['education' => $data->education])
-            @include('pages.sections.portfolio', ['portfolio' => $data->portfolio])
-            @include('pages.sections.certificates', ['certificates' => $data->certificates])
-            @include('pages.sections.passion', ['passion' => $data->passion])
-            @include('pages.sections.other_exp', ['other_exp' => $data->other_exp])
-            @include('pages.sections.recommandations', ['recommandations' => $data->recommandations])
-            @include('pages.sections.lets_talk', ['lets_talk' => $data->lets_talk]) --}}
     </div>
 
     @include('layout.footer')
--- resources/views/pages/sections/experiences.blade.php
+++ resources/views/pages/sections/experiences.blade.php
@@ -1,38 +1,42 @@
-<div class="section experiences" id="experiences">
-    <div class="container">
-        <div class="row">
-            <hr class="section-title-line">
-            <h1 class="section-title uppercase">experiences</h1>
-        </div>
-        <div class="row">
-            <div class="timeline">
-                @foreach ($experiences->items as $experience)
-                    <div class="section {{ $experience->classList }} rotated">
-                        <div class="content">
-                            <h2 class="where uppercase">{{ $experience->company }}</h2>
-                            <h2 class="function">{{ $experience->job }}</h2>
-                            <span><i class="far fa-calendar-alt"></i> {{ $experience->period }} |
-                                {!! calculateDate($experience->start_date, $experience->end_date) !!}
-                            </span>
-                            <hr class="mgtb-10">
-                            {!! $experience->content !!}
-                            <hr class="mgtb-10">
-                            {!! $experience->techs !!}
-                        </div>
-                    </div>
-                @endforeach
-
-            <!-- <div class="section code rotated freelance">
-                    <div class="content">
-                        <h2 class="where uppercase">Freelance</h2>
-                        <h2 class="function">Full Stack Developer</h2>
-                        <span><i class="far fa-calendar-alt"></i> Jan 2018 |
-                            <?php // echo calculateDate("2018-01");?>
-                </span>
-                <p>After an unforgettable experience as a programming teacher for children,
-                    I decided to switch back to programming and code.</p>
+<div class="py-5" id="experiences">
+    <div class="section experiences"">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col-md-10 offset-md-1 col-12 d-flex flex-column align-items-center justify-content-center">
+                    <hr class="section-title-line">
+                    <h1 class="section-title text-uppercase">experiences</h1>
+                </div>
             </div>
-        </div> -->
+            <div class="row">
+                <div class="timeline">
+                    @foreach ($experiences->items as $experience)
+                        <div class="section {{ $experience->classList }}">
+                            <div class="content">
+                                <h2 class="where text-uppercase">{{ $experience->company }}</h2>
+                                <h2 class="function">{{ $experience->job }}</h2>
+                                <span><i class="far fa-calendar-alt"></i> {{ $experience->period }} |
+                                    {!! calculateDate($experience->start_date, $experience->end_date) !!}
+                                </span>
+                                <hr class="my-2">
+                                {!! $experience->content !!}
+                                <hr class="my-2">
+                                {!! $experience->techs !!}
+                            </div>
+                        </div>
+                    @endforeach
+
+                <!-- <div class="section code rotated freelance">
+                        <div class="content">
+                            <h2 class="where uppercase">Freelance</h2>
+                            <h2 class="function">Full Stack Developer</h2>
+                            <span><i class="far fa-calendar-alt"></i> Jan 2018 |
+                                <?php // echo calculateDate("2018-01");?>
+                    </span>
+                    <p>After an unforgettable experience as a programming teacher for children,
+                        I decided to switch back to programming and code.</p>
+                </div>
+            </div> -->
+                </div>
             </div>
         </div>
     </div>
