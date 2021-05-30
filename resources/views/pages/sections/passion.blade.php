4dd7b090ac5369cb48ccc88fd85db5a02fca9426
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-30 21:26:15 +0100

    Edit passion items layout

--- resources/views/pages/sections/passion.blade.php
+++ resources/views/pages/sections/passion.blade.php
@@ -11,22 +11,22 @@
                 <div class="row">
                     @foreach ($passion->items as $index => $passionItem)
                         <div class="col-md-6 col-12 mb-3">
-                            <div class="list-group-item passion-item tc-grey-light-bg">
+                            <div class="list-group-item passion-item py-3 tc-grey-light-bg">
                                 <!-- .lits-group-item-figure -->
-                                <div class="list-group-item-figure">
+                                <div class="list-group-item-figure passion-header mb-3">
                                     <div class="has-badge d-flex align-items-center">
                                         <div class="passion-img me-2">
                                             <img src="{{ asset('/assets/img/icons/' . $passionItem->icon) }}"
                                             alt="{{ $passionItem->title }}">
                                         </div>
-                                        <div class="passion-header">
-                                            <h2 class="capitalize">{{ $passionItem->title }}</h2>
+                                        <div class="passion-header-txt">
+                                            <h4 class="text-capitalize">{{ $passionItem->title }}</h4>
                                         </div>
                                     </div>
                                 </div><!-- .lits-group-item-figure -->
                                 <!-- .lits-group-item-body -->
-                                <div class="list-group-item-body">
-                                    <p class="card-subtitle mb-1">{!! $passionItem->content !!}</p>
+                                <div class="list-group-item-body passion-body ps-2">
+                                    <p class="card-subtitle">{!! $passionItem->content !!}</p>
                                 </div><!-- .lits-group-item-body -->
                             </div>
                         </div>
