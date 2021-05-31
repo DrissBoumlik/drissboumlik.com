226fd31f91236ba9823595a66d71edf015a47424
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-31 21:26:01 +0100

    Fix 'about' layout on mobile

--- resources/views/pages/partials/about.blade.php
+++ resources/views/pages/partials/about.blade.php
@@ -1,4 +1,4 @@
-<div id="about" class="about section-wrapper py-4rem">
+<div id="about" class="about section-wrapper py-md-4rem py-3rem">
     <div class="section about-header">
         <div id="particles-js" class="particles-js"></div>
         <div class="container">
@@ -18,7 +18,7 @@
                 </div>
             </div>
             <div class="row mt-4">
-                <div class="col-11 col-md-8 offset-md-2">
+                <div class="col-10 offset-1 col-md-8 offset-md-2">
                     <div class="welcome-message">
                         <div class="welcome-message-wrapper text-center tc-black-almost fs-4">
                             <div class="welcome text-capitalize">welcome</div>
