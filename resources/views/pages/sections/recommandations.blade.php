261b8ba1bdb7b4cfb741bac7f97add5853b362f8
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-31 21:27:05 +0100

    Adapt DOM for owl carousel

--- resources/views/pages/sections/recommandations.blade.php
+++ resources/views/pages/sections/recommandations.blade.php
@@ -9,39 +9,22 @@
             </div>
 
             <div class="row">
-                <div class='offset-md-1 col-md-10 col-12'>
-                    <div id="recommandations-carousel" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-indicators">
-                            @php $count = count($recommandations->items) @endphp
-                            @for($index = 0; $index < $count; $index++)
-                                <button type="button" data-bs-target="#recommandations-carousel" data-bs-slide-to="{{ $index }}"
-                                aria-current="true" aria-label="Slide {{ $index }}" class="btn-indicator {{ ($index == 0 ? 'active' : '') }}"></button>
-                            @endfor
-                        </div>
-                        <div class="carousel-inner">
-                            @foreach ($recommandations->items as $index => $recommandation)
-                                <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
-                                    <div class="carousel-item-wrapper row">
-                                        <div class="carousel-img col-12">
-                                            <img src="{{ asset('/assets/img/people/' . $recommandation->icon) }}"
-                                            class="d-block square-100 rounded-circle" alt="...">
-                                        </div>
-                                        <div class="carousel-txt col-12">
-                                            {!! $recommandation->content !!}
-                                        </div>
-                                    </div>
+                <div class="owl-carousel owl-theme">
+                    @foreach ($recommandations->items as $index => $recommandation)
+                        @if (!isset($recommandation->hidden) || !$recommandation->hidden)
+                        <div class="owl-carousel-item item">
+                            <div class="owl-carousel-item-wrapper row">
+                                <div class="owl-carousel-img col-12">
+                                    <img src="{{ asset('/assets/img/people/' . $recommandation->icon) }}"
+                                    class="d-block square-100 rounded-circle" alt="...">
                                 </div>
-                            @endforeach
+                                <div class="owl-carousel-txt col-12">
+                                    {!! $recommandation->content !!}
+                                </div>
+                            </div>
                         </div>
-                        {{-- <button class="carousel-control-prev" type="button" data-bs-target="#recommandations-carousel" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#recommandations-carousel" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button> --}}
-                    </div>
+                        @endif
+                    @endforeach
                 </div>
             </div>
         </div>
