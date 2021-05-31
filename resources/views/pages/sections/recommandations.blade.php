09e5ddb940b41515f4b275950f487714a5acd4e2
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-31 01:43:01 +0100

    Add recommandations section

--- resources/views/pages/sections/recommandations.blade.php
+++ resources/views/pages/sections/recommandations.blade.php
@@ -1,49 +1,46 @@
-<div class="section recommandations block-inverse" id="recommandations">
-    <div class="container">
-        <div class="row">
-            <hr class="section-title-line">
-            <h1 class="section-title uppercase">{{ $recommandations->header }}</h1>
-        </div>
-        <div class='row'>
-            <div class='col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10'>
-                <div class="carousel slide recommandations-carousel" data-ride="carousel" id="quote-carousel-1">
-                    <!-- Bottom Carousel Indicators -->
-                    <ol class="carousel-indicators">
-                        @php $count = count($recommandations->items) @endphp
-                        @for($index = 0; $index < $count; $index++)
-                            <li data-target="#quote-carousel-1" data-slide-to="{{ $index }}"
-                                {{ ($index == 0 ? 'class=active' : '') }}></li>
-                        @endfor
-                    </ol>
+<div class="py-5" id="recommandations">
+    <div class="section recommandations">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col-md-10 offset-md-1 col-12 d-flex flex-column align-items-center justify-content-center">
+                    <hr class="section-title-line">
+                    <h1 class="section-title text-uppercase">{{ $recommandations->header }}</h1>
+                </div>
+            </div>
 
-                    <!-- Carousel Slides / Quotes -->
-                    <div class="carousel-inner">
-                        @foreach ($recommandations->items as $index => $recommandation)
-                            <div class="item {{ $index == 0 ? 'active' : '' }}">
-                                <blockquote>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-center">
-                                            <img class="img-circle" src="{{ $recommandation->icon }}">
+            <div class="row">
+                <div class='offset-md-1 col-md-10 col-12'>
+                    <div id="recommandations-carousel" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            @php $count = count($recommandations->items) @endphp
+                            @for($index = 0; $index < $count; $index++)
+                                <button type="button" data-bs-target="#recommandations-carousel" data-bs-slide-to="{{ $index }}"
+                                aria-current="true" aria-label="Slide {{ $index }}" class="btn-indicator {{ ($index == 0 ? 'active' : '') }}"></button>
+                            @endfor
+                        </div>
+                        <div class="carousel-inner">
+                            @foreach ($recommandations->items as $index => $recommandation)
+                                <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                                    <div class="carousel-item-wrapper row">
+                                        <div class="carousel-img col-12">
+                                            <img src="{{ asset('/assets/img/people/' . $recommandation->icon) }}"
+                                            class="d-block square-100 rounded-circle" alt="...">
                                         </div>
-                                        <div class="col-sm-9">
+                                        <div class="carousel-txt col-12">
                                             {!! $recommandation->content !!}
                                         </div>
                                     </div>
-                                </blockquote>
-                            </div>
-                        @endforeach
-                    </div>
-
-                    <!-- Carousel Buttons Next/Prev -->
-                    <div class="left-say left carousel-control opaque">
-                        <a data-slide="prev" href="#quote-carousel-1" class=""><i class="fa fa-chevron-left"></i>
-                            <!-- <span class="control-bg"></span> -->
-                        </a>
-                    </div>
-                    <div class="right-say right carousel-control opaque">
-                        <a data-slide="next" href="#quote-carousel-1" class=""><i class="fa fa-chevron-right"></i>
-                            <!-- <span class="control-bg"></span> -->
-                        </a>
+                                </div>
+                            @endforeach
+                        </div>
+                        {{-- <button class="carousel-control-prev" type="button" data-bs-target="#recommandations-carousel" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#recommandations-carousel" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button> --}}
                     </div>
                 </div>
             </div>
