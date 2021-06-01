0273375d4d2f3f3502a6528b057fb9035fa5346d
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-01 06:15:50 +0100

    Add cv link + Handle local/external link route

--- resources/views/layout/footer-parts/social-links.blade.php
+++ resources/views/layout/footer-parts/social-links.blade.php
@@ -3,7 +3,7 @@
         @foreach ($data->socialLinks as $socialLink)
             @if (!isset($socialLink->hidden) || !$socialLink->hidden)
                 <li class="list-group-item border-0 overflow-auto my-0 mx-2">
-                    <a href="/social/{{ $socialLink->link }}" target="_blank" rel="noopener"
+                    <a href="/{{ $socialLink->link }}/{{ (!isset($socialLink->local) || !$socialLink->local) ? '' : \App::getLocale() }}" target="_blank" rel="noopener"
                         aria-label="{{ $socialLink->title }}"
                         target="{{ $link->target ?? '_self' }}"
                         class="text-decoration-none">
