010127c7b1b4ccfedcefdef6740d81f4a682b82c
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-02 22:56:30 +0100

    Decrease the footer padding

--- resources/views/layout/footer-parts/copy-right.blade.php
+++ resources/views/layout/footer-parts/copy-right.blade.php
@@ -1,4 +1,4 @@
-<div class="copy-right-wrapper container w-100 py-5">
+<div class="copy-right-wrapper container w-100 py-4">
     <div class="row">
         <div class="col-lg-6 offset-lg-3 col-12 copyright-txt">
             <div class="copy-right">
