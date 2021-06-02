1fa8610af2ccd0630a5de038fe30ff931cc84f3b
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-02 06:30:20 +0100

    Add github link

--- config/properties.php
+++ config/properties.php
@@ -7,5 +7,6 @@ return [
         'instagram' => 'https://instagram.com/drissboumlik',
         'twitter' => 'https://twitter.com/drissboumlik',
         'linkedin' => 'https://www.linkedin.com/in/drissboumlik',
+        'github' => 'https://www.github.com/drissboumlik',
     ]
 ];
