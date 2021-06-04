9e273e3b2a6f9d23911f10c09f6a6dfcc16d8881
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-04 03:00:43 +0100

    Fix issues for portfolio section

--- resources/views/pages/sections/portfolio.blade.php
+++ resources/views/pages/sections/portfolio.blade.php
@@ -1,21 +1,27 @@
-<div class="section portfolio" id="portfolio">
-    <div class="container">
-        <div class="row">
-            <hr class="section-title-line">
-            <h1 class="section-title uppercase">{{ $portfolio->header }}</h1>
-        </div>
+<div class="py-5" id="portfolio">
+    <div class="section portfolio">
         <div class="container">
-            <div class="row">
-                @foreach ($portfolio->items as $portfolioItem)
-                    <div class="col-sm-4 col-md-4">
-                        <div class="box small">
-                            <div class="wrapper"><img src="{{ $portfolioItem->image }}" alt=""></div>
-                            <div class="tuto-title">
-                                {!! $portfolioItem->content !!}
+            <div class="row section-header">
+                <div class="col-md-10 offset-md-1 col-12 d-flex flex-column align-items-center justify-content-center">
+                    <hr class="section-title-line">
+                    <h1 class="section-title uppercase">{{ $portfolio->header }}</h1>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    @foreach ($portfolio->items as $portfolioItem)
+                        <div class="col-sm-4 col-md-4">
+                            <div class="box small">
+                                <div class="wrapper">
+                                    <img class="w-100" src="{{ asset('/assets/img/portfolio/' . $portfolioItem->image) }}" alt="">
+                                </div>
+                                <div class="tuto-title">
+                                    {!! $portfolioItem->content !!}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
