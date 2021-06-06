7441b39b9b5553e6312991773f22adadcd110ce3
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-06 16:42:48 +0100

    Add text drawing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
 
-        {{-- @include('addons.brand-text') --}}
+        @include('addons.brand-text')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="This is my resume.">
