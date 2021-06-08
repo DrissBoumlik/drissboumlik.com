e653297d14833239e7dee1ce2c3d10977ea3ccdf
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-08 18:16:56 +0100

    Organize external routes properties

--- config/properties.php
+++ config/properties.php
@@ -3,13 +3,17 @@
 return [
     "links" => [
         // Social links
-        'facebook' => 'https://facebook.com/drissboumlik',
-        'instagram' => 'https://instagram.com/drissboumlik',
-        'twitter' => 'https://twitter.com/drissboumlik',
-        'linkedin' => 'https://www.linkedin.com/in/drissboumlik',
-        'github' => 'https://www.github.com/drissboumlik',
-        'meet' => 'https://calendly.com/drissboumlik/30min',
-        'email' => 'mailto:idrissboumlik@gmail.com?subject=Resume : ',
-        'site' => 'https://teacode.ma'
+        'facebook' => 'https://facebook.com/drissboumlik/',
+        'instagram' => 'https://instagram.com/drissboumlik/',
+        'twitter' => 'https://twitter.com/drissboumlik/',
+        'linkedin' => 'https://www.linkedin.com/in/drissboumlik/',
+        'github' => 'https://www.github.com/drissboumlik/',
+
+        // Contact
+        'meet' => 'https://calendly.com/drissboumlik/30min/',
+        'email' => 'mailto:idrissboumlik@gmail.com?subject=Resume : /',
+
+        // Brand
+        'site' => 'https://teacode.ma/'
     ]
 ];
