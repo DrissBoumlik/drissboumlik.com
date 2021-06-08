37fc2129cd8a598e3aa0f65ff6b73f39e5cf5d0d
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-08 05:21:07 +0100

    Add preload for assets

--- resources/views/layout/header-parts/assets.blade.php
+++ resources/views/layout/header-parts/assets.blade.php
@@ -2,6 +2,7 @@
 <!-- Fonts -->
 <link rel="preload" href="{{ asset('/webfonts/fa-solid-900.woff2') }}" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="{{ asset('/webfonts/fa-brands-400.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset('/webfonts/fa-regular-400.woff2') }}" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="{{ asset('/assets/fonts/istokweb.woff2') }}" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="{{ asset('/assets/fonts/UntitledSansWeb-Bold.woff2') }}" as="font" type="font/woff2" crossorigin>
 
