02167fa5df234ca4f5be15c1a0a4608ffc7ae25a
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-22 23:11:37 +0100

    Move resume files to resume directory

--- resources/views/pages/resume/index.blade.php
+++ resources/views/pages/resume/index.blade.php
@@ -11,7 +11,7 @@
             @foreach ($data->sections as $key => $section)
             @php $i = array_search($key,array_keys($data->sections)) @endphp
             <div class="{{ $key }} section">
-                @include('pages.sections.'.$key, [$key => $section])
+                @include('pages.resume.sections.'.$key, [$key => $section])
             </div>
             @endforeach
         </div>
