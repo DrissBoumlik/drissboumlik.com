733510454cc1980a4d4a3f342d3db755fb89886f
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-23 06:19:59 +0100

    Refactor menu styles header/footer

--- resources/views/layout/footer-parts/footer-menu.blade.php
+++ resources/views/layout/footer-parts/footer-menu.blade.php
@@ -1,11 +1,11 @@
-<div class="footer-menu-wrapper container w-100 py-4">
+<div class="footer-menu-wrapper menu-wrapper container w-100 py-4">
     <div class="row">
         <div class="col-12 col-md-8 col-lg-8 offset-md-2 offset-lg-2">
             <div>
-                <ul class="footer-menu list-group list-group-horizontal align-items-start">
+                <ul class="footer-menu menu list-group list-group-horizontal align-items-start">
                     @foreach ($data->footerMenu as $link)
                         @if (!isset($link->hidden) || !$link->hidden)
-                            <li class="footer-menu-item list-group-item overflow-auto my-2 mx-3">
+                            <li class="footer-menu-item menu-item list-group-item overflow-auto my-2 mx-3">
                                 <a href="/{{ $link->slug }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
                                     aria-label="{{ $link->title }}" class="text-capitalize">
                                     {{ $link->title }}
--- resources/views/layout/header-parts/header-menu.blade.php
+++ resources/views/layout/header-parts/header-menu.blade.php
@@ -1,12 +1,14 @@
-<ul class="header-menu list-group list-group-horizontal align-items-start mt-3">
-    @foreach ($data->headerMenu as $link)
-        @if (!isset($link->hidden) || !$link->hidden)
-            <li class="header-menu-item list-group-item overflow-auto my-2 mx-2">
-                <a href="/{{ $link->slug }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
-                    aria-label="{{ $link->title }}" class="text-capitalize">
-                    {{ $link->title }}
-                </a>
-            </li>
-        @endif
-    @endforeach
-</ul>
+<div class="header-menu-wrapper menu-wrapper">
+    <ul class="header-menu menu list-group list-group-horizontal align-items-start mt-3">
+        @foreach ($data->headerMenu as $link)
+            @if (!isset($link->hidden) || !$link->hidden)
+                <li class="header-menu-item menu-item list-group-item overflow-auto my-2 mx-2">
+                    <a href="/{{ $link->slug }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
+                        aria-label="{{ $link->title }}" class="text-capitalize">
+                        {{ $link->title }}
+                    </a>
+                </li>
+            @endif
+        @endforeach
+    </ul>
+</div>
