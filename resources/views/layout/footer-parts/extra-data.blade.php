4f9ddfd3a4a84e5b3e94248d035d7419bc042196
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-23 06:42:23 +0100

    Fix typo

--- resources/views/layout/footer-parts/extra-data.blade.php
+++ resources/views/layout/footer-parts/extra-data.blade.php
@@ -21,7 +21,7 @@
 
             <div class="social-icons-wrapper">
                 <ul class="list-group list-group-horizontal align-items-start">
-                    @foreach ($data->socialLinks->teacode as $socialLink)
+                    @foreach ($data->socialLinks as $socialLink)
                         <li class="list-group-item border-0 overflow-auto my-0 mx-2">
                             <a href="/{{ $socialLink->link }}" target="_blank" rel="noopener"
                                 aria-label="{{ $socialLink->title }}"
