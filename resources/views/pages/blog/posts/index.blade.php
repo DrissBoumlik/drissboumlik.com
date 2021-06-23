bb2e620ce37da547a0765983b45a6e0b4ae5a53d
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-23 06:23:01 +0100

    Remove unused files

--- resources/views/pages/blog/posts/index.blade.php
+++ resources/views/pages/blog/posts/index.blade.php
@@ -1,12 +1,5 @@
 @extends('app')
 
-@section('header-assets')
-    <script src="https://unpkg.com/react@16.8.6/umd/react.production.min.js"></script>
-    <script src="https://unpkg.com/react-dom@16.8.6/umd/react-dom.production.min.js"></script>
-
-    <link rel="stylesheet" href="{{asset('vendor/laraberg/css/laraberg.css')}}">
-    <script src="{{ asset('vendor/laraberg/js/laraberg.js') }}"></script>
-@endsection
 
 @section('content')
     <div class="container-fluid p-0">
