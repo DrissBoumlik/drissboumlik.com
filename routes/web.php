516a4026510f5ab09f987494987cdd1b5bdf42b9
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-30 04:30:39 +0100

    Fix laravel voyager routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('cache.headers:public;max_age=15811200;etag')->group(function () {
 
-    Route::get('/posts/create', 'PostController@create');
+    Route::group(['prefix' => 'admin'], function () {
+        Voyager::routes();
+    });
+
 
     Route::get('/category/{category}', 'PostController@getPostsByCategory');
     Route::get('/tags/{tag}', 'PostController@getPostsByTag');
