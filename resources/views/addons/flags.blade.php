759f241e7b9014d78aaccd3b9c046453f2ff1bd5
Driss <drissboumlik.dev@gmail.com>
2022-06-29 21:49:23 +0100

    Refactor lang control

--- resources/views/addons/flags.blade.php
+++ resources/views/addons/flags.blade.php
@@ -1,12 +1,6 @@
 <div class="flags">
-    @if (\App::getLocale() != 'fr')
-        <a href="/resume/?lang=fr" class="flag-link">
-            <div class="flag flag-fr d-block">fr</div>
-        </a>
-    @endif
-    @if (\App::getLocale() != 'en')
-        <a href="/resume/?lang=en" class="flag-link">
-            <div class="flag flag-en d-block">en</div>
-        </a>
-    @endif
+    @php $locale = \App::getLocale() == 'fr' ? 'en' : 'fr' @endphp
+    <a href="/resume/?lang={{ $locale }}" class="flag-link">
+        <div class="flag flag-{{ $locale }} d-block">{{ $locale }}</div>
+    </a>
 </div>
