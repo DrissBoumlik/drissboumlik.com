d852d1729f8e2be25733da14d539e71ff0655d4c
Driss <drissboumlik.dev@gmail.com>
2023-07-09 16:26:08 +0100

    Add exp item & fix tiny style typos

--- resources/views/addons/social-links.blade.php
+++ resources/views/addons/social-links.blade.php
@@ -2,7 +2,7 @@
     <ul class="list-group list-group-horizontal align-items-start">
         @foreach ($data->socialLinks as $socialLink)
             @if (!isset($socialLink->hidden) || !$socialLink->hidden)
-                <li class="list-group-item border-0 overflow-auto my-0 mx-2">
+                <li class="list-group-item border-0 my-0 mx-2">
                     <a href="/{{ $socialLink->link }}" target="_blank" rel="noopener"
                         aria-label="{{ $socialLink->title }}" title="{{ $socialLink->title }}"
                         target="{{ $link->target ?? '_self' }}"
