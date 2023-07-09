cfed72b350fefec61d489449cabc864092a010d4
Driss <drissboumlik.dev@gmail.com>
2023-07-09 17:00:23 +0100

    Remove laravel voyager

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,8 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
-use TCG\Voyager\Models\Role;
-use TCG\Voyager\Models\User;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,14 +16,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         if (User::count() == 0) {
-            $role = Role::where('name', 'admin')->firstOrFail();
-
             User::create([
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
                 'password'       => bcrypt('password'),
                 'remember_token' => Str::random(60),
-                'role_id'        => $role->id,
             ]);
         }
     }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('cache.headers:public;max_age=15811200;etag')->group(function () {
 
-    Route::group(['prefix' => 'admin'], function () {
-        Voyager::routes();
-    });
-
 
     Route::get('/category/{category}', 'PostController@getPostsByCategory');
     Route::get('/tags/{tag}', 'PostController@getPostsByTag');
