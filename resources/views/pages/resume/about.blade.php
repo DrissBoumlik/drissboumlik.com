2665ae5c24676f1420f9899eec43cb5a3c8a1969
Driss <drissboumlik.dev@gmail.com>
2023-07-27 15:25:45 +0100

    Remove about text

--- resources/views/pages/resume/about.blade.php
+++ resources/views/pages/resume/about.blade.php
@@ -1,4 +1,4 @@
-<div id="about" class="about section-wrapper py-md-4rem py-3rem">
+<div id="about" class="about section-wrapper py-5">
     <div class="section about-header">
         <div id="particles-js" class="particles-js"></div>
         <div class="container">
@@ -15,24 +15,24 @@
                     @include('layout.header-parts.header-menu')
                 </div>
             </div>
-            <div class="row">
-                <div class="col-10 offset-1 col-md-8 offset-md-2">
-                    <div class="welcome-message">
-                        <div class="welcome-message-wrapper text-center tc-black-almost fs-4">
-                            <div class="welcome text-capitalize">welcome</div>
-                            <div class="txt">
-                                <div class="">
-                                    <p class="capitalize-first-letter">this is my resume ... ish</p>
-                                    <p class="capitalize-first-letter">
-                                        {{-- <span>{{ $data->general['whois']}} </span> --}}
-                                        <span class="text-capitalize funny-font">driss boumlik</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="row">--}}
+{{--                <div class="col-10 offset-1 col-md-8 offset-md-2">--}}
+{{--                    <div class="welcome-message">--}}
+{{--                        <div class="welcome-message-wrapper text-center tc-black-almost fs-4">--}}
+{{--                            <div class="welcome text-capitalize">welcome</div>--}}
+{{--                            <div class="txt">--}}
+{{--                                <div class="">--}}
+{{--                                    <p class="capitalize-first-letter">this is my resume ... ish</p>--}}
+{{--                                    <p class="capitalize-first-letter">--}}
+{{--                                        --}}{{-- <span>{{ $data->general['whois']}} </span> --}}
+{{--                                        <span class="text-capitalize funny-font">driss boumlik</span>--}}
+{{--                                    </p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
             <div class="row mt-3">
                 <div class="col-12">
                     <div class="social-icons">
