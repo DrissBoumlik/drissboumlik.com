b6f96198659ff57ed02e34a6cc1e14dcb3f26682
Driss <drissboumlik.dev@gmail.com>
2023-07-27 15:28:04 +0100

    Add styles & Edit error pages layout

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,27 +1,30 @@
-@extends('admin.template.frontend')
+@extends('layout.page-content')
 
-@section('content')
-    <!-- Page Content -->
-    <div class="hero h-auto">
-        <div class="hero-inner text-center">
-            <div class="bg-body-extra-light">
-                <div class="content content-full overflow-hidden">
-                    <div class="row">
-                        <div class="col-md-6 offset-md-3">
-                            <div class="py-4">
-                                <!-- Error Header -->
-                                <h1 class="display-1 fw-bolder text-city">404</h1>
-                                <h2 class="fw-normal text-muted mb-4">
-                                    We are sorry but the page you are looking for was not found..
-                                </h2>
-                                <!-- END Error Header -->
+@section('page-content')
+
+    <div class="container-fluid p-0">
+
+        <div class="_404">
+            <div class="row my-5">
+                <div class="col-lg-8 offset-lg-2 col-12">
+                    <div class="not-found-container">
+                        <div class="not-found-bar">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div class="not-found-header"></div>
+                        <div class="not-found-body">
+                            <div class="row"><i class="fa-solid fa-face-frown"></i></div>
+                            <div class="row">
+                                <p class="not-found-txt">page not found</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
-    <!-- END Page Content -->
 
 @endsection
