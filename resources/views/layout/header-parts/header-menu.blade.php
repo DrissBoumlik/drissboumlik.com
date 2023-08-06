5c6a7f48cf8117679ace5e395b803898aef9c85a
Driss <drissboumlik.dev@gmail.com>
2023-08-06 13:11:07 +0100

    Emphasise active header menu link

--- resources/views/layout/header-parts/header-menu.blade.php
+++ resources/views/layout/header-parts/header-menu.blade.php
@@ -1,7 +1,8 @@
 <div class="header-menu-wrapper menu-wrapper">
     <ul class="header-menu menu list-group list-group-horizontal align-items-start">
         @foreach ($data->headerMenu as $link)
-            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto my-2 mx-2">
+            <li class="header-menu-item menu-item list-group-item animated-underline
+                    overflow-auto my-2 mx-2 {{ request()->is($link->slug) ? 'active' : '' }}">
                 <a href="/{{ $link->slug }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
                     aria-label="{{ $link->title }}" class="text-capitalize">
                     {{ $link->title }}
