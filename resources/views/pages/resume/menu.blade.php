489d72147e5e6062e7c90c7b20200bb8d54fdcb4
Driss <drissboumlik.dev@gmail.com>
2023-10-15 16:18:28 +0100

    Edit resume menu icons

--- resources/views/pages/resume/menu.blade.php
+++ resources/views/pages/resume/menu.blade.php
@@ -10,19 +10,19 @@
             <a href="/resume#languages"><i class="fa-solid fa-language"></i></a>
         </li>
         <li class="list-group-item" title="Additionnel Skills" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#add-skills"><i class="fa-solid fa-futbol"></i></a>
+            <a href="/resume#add-skills"><i class="fa-solid fa-circle-plus"></i></a>
         </li>
         <li class="list-group-item" title="Education" data-toggle="tooltip" data-placement="left">
             <a href="/resume#education"><i class="fa-solid fa-user-graduate"></i></a>
         </li>
         <li class="list-group-item" title="Passion" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#passion"><i class="fa-solid fa-heart"></i></a>
+            <a href="/resume#passion"><i class="fa-solid fa-plug-circle-check"></i></a>
         </li>
         <li class="list-group-item" title="Other Experiences" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#other-experiences"><i class="fa-solid fa-book"></i></a>
+            <a href="/resume#other-experiences"><i class="fa-solid fa-person-digging"></i></a>
         </li>
         <li class="list-group-item" title="What People Say" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#recommandations"><i class="fa-solid fa-star"></i></a>
+            <a href="/resume#recommandations"><i class="fa-solid fa-comments"></i></a>
         </li>
     </ul>
 </div>
