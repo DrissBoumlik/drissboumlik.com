2c9867c1750829a2cef166795a4f8d6fd119dbc3
Driss <drissboumlik.dev@gmail.com>
2023-10-22 21:42:04 +0100

    Add services section to the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -35,8 +35,92 @@
                 </div>
             </div>
             <div class="section">
+                <div class="services">
+                    <div class="section py-5">
+                        <div class="container">
+                            <div class="row section-header">
+                                <div class="col-md-10 offset-md-1 col-12 d-flex flex-column align-items-center justify-content-center">
+                                    <hr class="section-title-line">
+                                    <h1 class="section-title">Services</h1>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xl-4 col-lg-4 col-12 mb-4">
+                                    <div class="service-box w-100">
+                                        <div class="service-icon">
+                                            <i class="fa-solid fa-palette"></i>
+                                        </div>
+                                        <div class="service-title">
+                                            <h3>UI/UX Design</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 col-lg-4 col-12 mb-4">
+                                    <div class="service-box w-100">
+                                        <div class="service-icon">
+                                            <i class="fa-solid fa-code"></i>
+                                        </div>
+                                        <div class="service-title">
+                                            <h3>Web Development</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 col-lg-4 col-12 mb-4">
+                                    <div class="service-box w-100">
+                                        <div class="service-icon">
+                                            <i class="fa-solid fa-laptop-code"></i>
+                                        </div>
+                                        <div class="service-title">
+                                            <h3>Workshops</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section">
                 @include('pages.resume.sections.recommendations', ['recommendations' => $data->sections['recommendations']])
             </div>
+{{--            <div class="section">--}}
+{{--                <div class="get-in-touch">--}}
+{{--                    <div class="section py-5">--}}
+{{--                        <div class="container">--}}
+{{--                            <div class="row section-header">--}}
+{{--                                <div class="col-md-10 offset-md-1 col-12 d-flex flex-column align-items-center justify-content-center">--}}
+{{--                                    <hr class="section-title-line">--}}
+{{--                                    <h1 class="section-title">Get in Touch</h1>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-6 offset-3">--}}
+{{--                                    <form action="{{ route('get-in-touch') }}" method="POST">--}}
+{{--                                        @csrf--}}
+{{--                                        <div class="mb-3">--}}
+{{--                                            <label for="form-name" class="form-label">Name</label>--}}
+{{--                                            <input type="text" class="form-control" id="form-name" placeholder="" name="name" value="blabla" />--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mb-3">--}}
+{{--                                            <label for="form-email" class="form-label">Email address</label>--}}
+{{--                                            <input type="email" class="form-control" id="form-email" placeholder="" name="email" value="blabla@bla.bla" />--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mb-3">--}}
+{{--                                            <label for="form-subject" class="form-label">Subject</label>--}}
+{{--                                            <input type="text" class="form-control" id="form-subject" placeholder="" name="subject" value="blabla" />--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mb-3">--}}
+{{--                                            <label for="form-message" class="form-label">Message</label>--}}
+{{--                                            <textarea class="form-control" id="form-message" rows="3" name="message">sdfkjsdfjhj</textarea>--}}
+{{--                                        </div>--}}
+{{--                                        <button type="submit" class="btn btn-primary">Submit</button>--}}
+{{--                                    </form>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 
