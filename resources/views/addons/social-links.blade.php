d208d64fcd12540a7c5510223f398d91ddcd2a41
Driss <drissboumlik.dev@gmail.com>
2023-10-22 20:31:25 +0100

    Fix slug link handling with URL facade

--- resources/views/addons/social-links.blade.php
+++ resources/views/addons/social-links.blade.php
@@ -3,7 +3,7 @@
         @foreach ($data->socialLinks as $socialLink)
             @if (!isset($socialLink->hidden) || !$socialLink->hidden)
                 <li class="list-group-item border-0 my-0 mx-2">
-                    <a href="/{{ $socialLink->link }}" target="_blank" rel="noopener"
+                    <a href="{{ \URL::to($socialLink->link) }}" target="_blank" rel="noopener"
                         aria-label="{{ $socialLink->title }}" title="{{ $socialLink->title }}"
                         target="{{ $link->target ?? '_self' }}"
                         class="text-decoration-none">
