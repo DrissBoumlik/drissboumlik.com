c471ae1f2572e2737f872ab9e043bb975e4224a3
Driss <drissboumlik.dev@gmail.com>
2023-10-24 23:33:46 +0100

    Remove testing values

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -98,19 +98,19 @@
                                         @csrf
                                         <div class="mb-3">
                                             <label for="form-name" class="form-label">Name</label>
-                                            <input type="text" class="form-control" id="form-name" placeholder="" name="name" value="blabla" />
+                                            <input type="text" class="form-control" id="form-name" placeholder="" name="name" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="form-email" class="form-label">Email address</label>
-                                            <input type="email" class="form-control" id="form-email" placeholder="" name="email" value="blabla@bla.bla" />
+                                            <input type="email" class="form-control" id="form-email" placeholder="" name="email" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="form-subject" class="form-label">Subject</label>
-                                            <input type="text" class="form-control" id="form-subject" placeholder="" name="subject" value="blabla" />
+                                            <input type="text" class="form-control" id="form-subject" placeholder="" name="subject" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="form-body" class="form-label">Message</label>
-                                            <textarea class="form-control" id="form-body" rows="3" name="body" maxlength="1000">sdfkjsdfjhj</textarea>
+                                            <textarea class="form-control" id="form-body" rows="3" name="body" maxlength="1000"></textarea>
                                         </div>
                                         <button type="submit" class="btn tc-blue-dark-1-bg tc-blue-dark-2-bg-hover w-100">Submit</button>
                                     </form>
