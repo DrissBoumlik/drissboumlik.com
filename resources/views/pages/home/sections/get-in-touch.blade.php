4c83e2145496bfe70f7c9f5163969e5fb56e9564
Driss <drissboumlik.dev@gmail.com>
2023-10-25 23:29:26 +0100

    Make contact fields required on client side

--- resources/views/pages/home/sections/get-in-touch.blade.php
+++ resources/views/pages/home/sections/get-in-touch.blade.php
@@ -12,15 +12,15 @@
                     @csrf
                     <div class="mb-3">
                         <label for="form-name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="form-name" placeholder="John Doe" name="name"/>
+                        <input type="text" class="form-control" id="form-name" placeholder="John Doe" name="name" required />
                     </div>
                     <div class="mb-3">
                         <label for="form-email" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="form-email" placeholder="jd@email.com" name="email"/>
+                        <input type="email" class="form-control" id="form-email" placeholder="jd@email.com" name="email" required />
                     </div>
                     <div class="mb-3">
                         <label for="form-body" class="form-label">Message</label>
-                        <textarea class="form-control" id="form-body" rows="3" name="body" placeholder="Your Message" maxlength="1000"></textarea>
+                        <textarea class="form-control" id="form-body" rows="3" name="body" placeholder="Your Message" required maxlength="1000"></textarea>
                     </div>
                     <button type="submit" class="btn tc-blue-dark-1-bg tc-blue-dark-2-bg-hover w-100">
                         Submit
