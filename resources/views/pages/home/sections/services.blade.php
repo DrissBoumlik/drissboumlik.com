c3693ec81fa92782270197f31ad3f1e2f3933c9e
Driss <drissboumlik.dev@gmail.com>
2023-10-26 23:58:12 +0100

    Edit services section style

--- resources/views/pages/home/sections/services.blade.php
+++ resources/views/pages/home/sections/services.blade.php
@@ -7,34 +7,34 @@
                 <h1 class="section-title">Services</h1>
             </div>
         </div>
-        <div class="row justify-content-center">
-            <div class="col-xl-4 col-lg-4 col-md-6 col-12 mb-4">
+        <div class="row justify-content-center mb-md-4">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-12">
                 <div class="service-box w-100">
                     <div class="service-icon">
                         <i class="fa-solid fa-palette"></i>
                     </div>
                     <div class="service-title">
-                        <h3>UI/UX Design</h3>
+                        <span>UI/UX Design</span>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 col-12 mb-4">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-12">
                 <div class="service-box w-100">
                     <div class="service-icon">
                         <i class="fa-solid fa-code"></i>
                     </div>
                     <div class="service-title">
-                        <h3>Web Development</h3>
+                        <span>Web Development</span>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 col-12 mb-4">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-12">
                 <div class="service-box w-100">
                     <div class="service-icon">
                         <i class="fa-solid fa-laptop-code"></i>
                     </div>
                     <div class="service-title">
-                        <h3>Workshops</h3>
+                        <span>Workshops</span>
                     </div>
                 </div>
             </div>
