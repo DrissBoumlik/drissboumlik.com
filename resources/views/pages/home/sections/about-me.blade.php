c4e86ecef13b72a1ada9ba54d2e2433b2bfcb885
Driss <drissboumlik.dev@gmail.com>
2023-10-29 23:17:10 +0100

    Fix about me line break

--- resources/views/pages/home/sections/about-me.blade.php
+++ resources/views/pages/home/sections/about-me.blade.php
@@ -14,7 +14,7 @@
                         <div class="welcome-message">
                             <div class="welcome-message-wrapper tc-black-almost">
                                 <div class="txt">
-                                    I code, blog, speak, write, teach ... and other stuff usually not in the same order
+                                    I code, blog, speak, write, teach ... and other stuff usually not in the same order.<br/>
                                     I'm a developer & teacher among other things who likes to build & share to make some positive difference.
                                 </div>
                             </div>
