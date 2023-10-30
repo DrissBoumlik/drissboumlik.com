99cb52498a99e74582d21ad8cf2324f81d59f20a
Driss <drissboumlik.dev@gmail.com>
2023-10-30 12:19:19 +0100

    Edit experience structure

--- resources/views/pages/resume/sections/experiences.blade.php
+++ resources/views/pages/resume/sections/experiences.blade.php
@@ -1,5 +1,5 @@
-<div class="py-5" id="experiences">
-    <div class="experiences">
+<div class="py-5">
+    <div class="section experiences" id="experiences">
         <div class="container">
             <div class="row section-header">
                 <div class="col-md-10 offset-md-1 col-12 d-flex flex-column align-items-center justify-content-center">
@@ -20,27 +20,29 @@
                                     </span>
                                     @if ($experience->content)
                                         <hr class="my-2">
-                                        {!! $experience->content !!}
+                                        @isset($experience->content->headline) {!! $experience->content->headline !!} @endisset
+                                        @if(isset($experience->content->items) && is_array($experience->content->items))
+                                            <ul>
+                                                @foreach($experience->content->items as $item)
+                                                    <li>{!! $item !!}</li>
+                                                @endforeach
+                                            </ul>
+                                        @endif
                                     @endif
-                                    @if ($experience->techs)
+                                    @if (isset($experience->techs) && is_array($experience->techs))
                                         <hr class="my-2">
-                                        {!! $experience->techs !!}
+                                        <div class="tech-env">
+                                            <div class="underline">Technical environment :</div>
+                                            <div class="tech-env-items">
+                                                @foreach($experience->techs as $tech)
+                                                    <span class="tech-tag tag">{!! $tech->title !!}</span>
+                                                @endforeach
+                                                </div>
+                                        </div>
                                     @endif
                                 </div>
                             </div>
                         @endforeach
-
-                    <!-- <div class="section code rotated freelance">
-                            <div class="content">
-                                <h2 class="where uppercase">Freelance</h2>
-                                <h2 class="function">Full Stack Developer</h2>
-                                <span><i class="far fa-calendar-alt"></i> Jan 2018 |
-                                    <?php // echo calculateDate("2018-01");?>
-                        </span>
-                        <p>After an unforgettable experience as a programming teacher for children,
-                            I decided to switch back to programming and code.</p>
-                    </div>
-                </div> -->
                     </div>
                 </div>
             </div>
