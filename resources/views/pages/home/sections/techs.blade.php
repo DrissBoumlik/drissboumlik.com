c1ec1862816d9114a195255c1ad31c9693c80bb0
Driss <drissboumlik.dev@gmail.com>
2023-10-30 17:31:04 +0100

    Add tech text under icons

--- resources/views/pages/home/sections/techs.blade.php
+++ resources/views/pages/home/sections/techs.blade.php
@@ -12,7 +12,7 @@
                 <div class="col-3">
                     <div class="skill-item">
                         <div class="skill-icon">{!! $c->icon !!}</div>
-{{--                        <div class="skill-text text-capitalize">{{ $c->name }}</div>--}}
+                        <div class="skill-text text-capitalize">{{ $c->name }}</div>
                     </div>
                 </div>
             @endforeach
