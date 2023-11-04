9ef4c9810f50933989488b0499e54dcb8b3c1d98
Driss <drissboumlik.dev@gmail.com>
2023-11-05 00:13:31 +0100

    Remove placeholders content from contact form

--- resources/views/pages/home/sections/get-in-touch.blade.php
+++ resources/views/pages/home/sections/get-in-touch.blade.php
@@ -6,15 +6,15 @@
                 <form id="contact-form" class="mb-4">
                     @csrf
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="form-name" placeholder="John Doe" name="name" required />
+                        <input type="text" class="form-control" id="form-name" placeholder="" name="name" required />
                         <label for="form-name">Name</label>
                     </div>
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="form-email" placeholder="jd@email.com" name="email" required />
+                        <input type="email" class="form-control" id="form-email" placeholder="" name="email" required />
                         <label for="form-email">Email address</label>
                     </div>
                     <div class="form-floating mb-3">
-                        <textarea class="form-control" id="form-body" rows="3" name="body" placeholder="Your Message" required maxlength="1000"></textarea>
+                        <textarea class="form-control" id="form-body" rows="3" name="body" placeholder="" required maxlength="1000"></textarea>
                         <label for="form-body">Message</label>
                     </div>
                     <button type="submit" class="btn tc-blue-dark-2-bg tc-blue-bg-hover w-100">Send</button>
