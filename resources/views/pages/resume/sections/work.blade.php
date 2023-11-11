61209c79cb5821dbacb766eeaac3df8965ddb485
Driss <drissboumlik.dev@gmail.com>
2023-11-12 00:52:01 +0100

    Hide work links (for now)

--- resources/views/pages/resume/sections/work.blade.php
+++ resources/views/pages/resume/sections/work.blade.php
@@ -9,24 +9,24 @@
                             <div class="work-box box mb-4">
                                 <div class="work-image-cover" style="background-image: url('{{ asset('/assets/img/work/' . $workItem->image) }}')">
                                 </div>
-                                @isset($workItem->links)
-                                    <div class="work-links">
-                                        @isset($workItem->links->repository)
-                                        <div class="work-link">
-                                            <a href="{{ $workItem->links->repository }}" target="_blank">
-                                                <i class="fa-brands fa-github"></i>
-                                            </a>
-                                        </div>
-                                        @endisset
-                                        @isset($workItem->links->website)
-                                        <div class="work-link">
-                                            <a href="{{ $workItem->links->website }}" target="_blank">
-                                                <i class="fa-solid fa-globe"></i>
-                                            </a>
-                                        </div>
-                                        @endisset
-                                    </div>
-                                @endisset
+{{--                                @isset($workItem->links)--}}
+{{--                                    <div class="work-links">--}}
+{{--                                        @isset($workItem->links->repository)--}}
+{{--                                        <div class="work-link">--}}
+{{--                                            <a href="{{ $workItem->links->repository }}" target="_blank">--}}
+{{--                                                <i class="fa-brands fa-github"></i>--}}
+{{--                                            </a>--}}
+{{--                                        </div>--}}
+{{--                                        @endisset--}}
+{{--                                        @isset($workItem->links->website)--}}
+{{--                                        <div class="work-link">--}}
+{{--                                            <a href="{{ $workItem->links->website }}" target="_blank">--}}
+{{--                                                <i class="fa-solid fa-globe"></i>--}}
+{{--                                            </a>--}}
+{{--                                        </div>--}}
+{{--                                        @endisset--}}
+{{--                                    </div>--}}
+{{--                                @endisset--}}
                             </div>
                         </div>
                     @endforeach
