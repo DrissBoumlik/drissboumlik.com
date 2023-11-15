6c11463f21b22e8a22aa51716cc63e10fca9b496
Driss <drissboumlik.dev@gmail.com>
2023-11-15 21:17:33 +0100

    Edit techs section layout

--- resources/views/pages/home/sections/techs.blade.php
+++ resources/views/pages/home/sections/techs.blade.php
@@ -1,9 +1,9 @@
 <div class="skills py-5">
     <div class="container">
-        @include('components.headline', ['headline' => $data->sections['techs']->header])
+        @include('components.headline', ['headline' => $techs->header])
         <div class="row">
-            @foreach($data->sections['techs']->items as $c)
-                <div class="col-3">
+            @foreach($techs->items as $c)
+                <div class="col-4 col-md-3">
                     <div class="skill-item">
                         <div class="skill-icon">{!! $c->icon !!}</div>
                         <div class="skill-text text-capitalize">{{ $c->name }}</div>
