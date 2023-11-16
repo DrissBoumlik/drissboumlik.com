e2922b5eecce4408c023fc1117fd833617a9b793
Driss <drissboumlik.dev@gmail.com>
2023-11-16 16:44:48 +0100

    Edit testimonials dom

--- resources/views/pages/testimonials.blade.php
+++ resources/views/pages/testimonials.blade.php
@@ -3,14 +3,8 @@
 
 @section('page-content')
     <div class="container-fluid p-0">
-        <div class="testimonials">
-            <div class="section py-5">
-                <div class="container">
-                    <div class="section no-slider">
-                        @include('pages.resume.sections.testimonials', ['testimonials' => $data->sections['testimonials']])
-                    </div>
-                </div>
-            </div>
+        <div class="testimonials section no-slider py-5">
+            @include('pages.resume.sections.testimonials', ['testimonials' => $data->sections['testimonials']])
         </div>
     </div>
 @endsection
