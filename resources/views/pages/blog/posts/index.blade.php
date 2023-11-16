e27bf8f5a34d2ce866fe9012ec970076341d7614
Driss <drissboumlik.dev@gmail.com>
2023-11-16 16:52:12 +0100

    Edit title position

--- resources/views/pages/blog/posts/index.blade.php
+++ resources/views/pages/blog/posts/index.blade.php
@@ -3,29 +3,31 @@
 
 @section('page-content')
     <div class="container-fluid p-0">
-        <div class="posts">
-            <div class="section py-5">
-                <div class="container">
-                    @include('components.headline', ['headline' => $data->headline])
-                    <div class="row">
-                        @foreach ($posts as $post)
-                            <div class="col-12 col-lg-6 col-xl-6 mb-4">
-                                @include('components.post', ['post' => $post])
-                            </div>
-                        @endforeach
-                        @if (!count($posts))
-                            <div class="col-12">
-                                <div class="message-wrapper d-flex justify-content-center align-items-center"
-                                     style="min-height: 300px">
-                                    <h3 class="text-uppercase">No posts found <i class="fa-solid fa-sad-cry"></i>!</h3>
+        <div class="posts section py-5">
+            <div class="py-5" id="posts">
+                <div class="section posts">
+                    <div class="container">
+                        @include('components.headline', ['headline' => $data->headline])
+                        <div class="row">
+                            @foreach ($posts as $post)
+                                <div class="col-12 col-lg-6 col-xl-6 mb-4">
+                                    @include('components.post', ['post' => $post])
                                 </div>
-                            </div>
-                        @endif
-                        <div class="col-12 col-md-8 offset-md-2
-                                    col-lg-8 offset-lg-2 col-xl-6 offset-xl-3
-                                    mt-5">
-                            <div class="pagination-wrapper justify-content-center">
-                                {{ $posts_data->onEachSide(5)->links() }}
+                            @endforeach
+                            @if (!count($posts))
+                                <div class="col-12">
+                                    <div class="message-wrapper d-flex justify-content-center align-items-center"
+                                         style="min-height: 300px">
+                                        <h3 class="text-uppercase">No posts found <i class="fa-solid fa-sad-cry"></i>!</h3>
+                                    </div>
+                                </div>
+                            @endif
+                            <div class="col-12 col-md-8 offset-md-2
+                                        col-lg-8 offset-lg-2 col-xl-6 offset-xl-3
+                                        mt-5">
+                                <div class="pagination-wrapper justify-content-center">
+                                    {{ $posts_data->onEachSide(5)->links() }}
+                                </div>
                             </div>
                         </div>
                     </div>
--- resources/views/pages/blog/tags/index.blade.php
+++ resources/views/pages/blog/tags/index.blade.php
@@ -3,35 +3,37 @@
 
 @section('page-content')
     <div class="container-fluid p-0">
-        <div class="tags">
-            <div class="section py-5">
-                <div class="container">
-                    @include('components.headline', ['headline' => $data->headline])
-                    <div class="row">
-                        @foreach ($tags as $tag)
-                                <div class="col-12 col-md-6 col-lg-4 mb-4">
-                                    <div class="tag-item">
-                                        <div class="tag-cover" style="background-image: url('/{{ $tag->cover }}')"></div>
-                                        <div class="tag-text">
-                                            <a href="/tags/{{ $tag->slug }}" class="tag-link text-decoration-none">
-                                                <div class="font-weight-bolder text-uppercase tag-link-text">{{ $tag->name }}</div>
-                                            </a>
+        <div class="tags section py-5">
+            <div class="py-5" id="tags">
+                <div class="section tags">
+                    <div class="container">
+                        @include('components.headline', ['headline' => $data->headline])
+                        <div class="row">
+                            @foreach ($tags as $tag)
+                                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                                        <div class="tag-item">
+                                            <div class="tag-cover" style="background-image: url('/{{ $tag->cover }}')"></div>
+                                            <div class="tag-text">
+                                                <a href="/tags/{{ $tag->slug }}" class="tag-link text-decoration-none">
+                                                    <div class="font-weight-bolder text-uppercase tag-link-text">{{ $tag->name }}</div>
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
+                                @endforeach
+                            @if (!count($tags))
+                                <div class="col-12">
+                                    <div class="message-wrapper d-flex justify-content-center align-items-center"
+                                         style="min-height: 300px">
+                                        <h3 class="text-uppercase">No tags found <i class="fa-solid fa-sad-cry"></i>!</h3>
+                                    </div>
                                 </div>
-                            @endforeach
-                        @if (!count($tags))
-                            <div class="col-12">
-                                <div class="message-wrapper d-flex justify-content-center align-items-center"
-                                     style="min-height: 300px">
-                                    <h3 class="text-uppercase">No tags found <i class="fa-solid fa-sad-cry"></i>!</h3>
+                            @endif
+                            <div class="col-12 col-md-8 offset-md-2
+                                        col-lg-8 offset-lg-2 col-xl-6 offset-xl-3 mt-5">
+                                <div class="pagination-wrapper justify-content-center">
+                                    {{ $tags->onEachSide(5)->links() }}
                                 </div>
-                            </div>
-                        @endif
-                        <div class="col-12 col-md-8 offset-md-2
-                                    col-lg-8 offset-lg-2 col-xl-6 offset-xl-3 mt-5">
-                            <div class="pagination-wrapper justify-content-center">
-                                {{ $tags->onEachSide(5)->links() }}
                             </div>
                         </div>
                     </div>
