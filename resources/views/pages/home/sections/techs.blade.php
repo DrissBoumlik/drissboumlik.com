b4f2a6d7abb52e878ddc2164a26f933ead50ae5e
Driss <drissboumlik.dev@gmail.com>
2023-11-19 15:25:34 +0100

    Edit techs layout and order

--- resources/views/pages/home/sections/techs.blade.php
+++ resources/views/pages/home/sections/techs.blade.php
@@ -3,7 +3,7 @@
         @include('components.headline', ['headline' => $techs->header])
         <div class="row">
             @foreach($techs->items as $c)
-                <div class="col-4 col-md-3">
+                <div class="col-4 col-sm-3 col-md-2">
                     <div class="skill-item">
                         <div class="skill-icon">{!! $c->icon !!}</div>
                         <div class="skill-text text-capitalize">{{ $c->name }}</div>
