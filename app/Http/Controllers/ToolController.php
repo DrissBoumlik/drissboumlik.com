a829a1d90900aee2d2b00f388440691f5ca7c9ee
Driss <drissboumlik.dev@gmail.com>
2023-11-20 13:20:31 +0100

    Add timestamp to exported db filename

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -8,14 +8,17 @@ class ToolController extends Controller
 {
     public function export_db(Request $request)
     {
-        $dumpPath = database_path('dumps/dump.sql');
+        $now = date("Y-m-d_h-i");
+        $db_name = env('DB_DATABASE');
+        $filename = $db_name . "_exported_at_$now.sql";
+        $dumpPath = database_path("dumps/$filename");
         \Spatie\DbDumper\Databases\MySql::create()
-            ->setDbName(env('DB_DATABASE'))
+            ->setDbName($db_name)
             ->setUserName(env('DB_USERNAME'))
             ->setPassword(env('DB_PASSWORD'))
             ->dumpToFile($dumpPath);
 
-        return \Response::download($dumpPath, "dump.sql", [
+        return \Response::download($dumpPath, $filename, [
             'Content-Type' => 'application/sql',
             // 'Content-Transfer-Encoding'=> 'binary',
             // 'Accept-Ranges'=> 'bytes'
