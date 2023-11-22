c5b7bb16c8458550c2face88d281149fa58ce921
Driss <drissboumlik.dev@gmail.com>
2023-11-22 16:28:22 +0100

    Add underline animation to admin links

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -6,7 +6,7 @@
                     <ul class="header-menu list-group list-group-horizontal">
                         @foreach ($headerMenu as $link)
                             <li class="header-menu-item menu-item list-group-item animated-underline
-                    overflow-auto my-2 mx-2 {{ request()->is($link->slug) ? 'active' : '' }}">
+                                        overflow-auto my-2 mx-2 {{ request()->is($link->slug) ? 'active' : '' }}">
                                 <a href="{{ \URL::to($link->slug) }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
                                    aria-label="{{ $link->title }}" class="text-capitalize">
                                     {!! $link->title !!}
@@ -14,13 +14,13 @@
                             </li>
                         @endforeach
                         @auth
-                            <li class="header-menu-item menu-item list-group-item overflow-auto my-2 mx-2">
+                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto my-2 mx-2">
                                 <a href="/admin" rel="noopener" target="_blank"
                                    aria-label="Admin Panel">
                                     <i class="fa-solid fa-gear"></i>
                                 </a>
                             </li>
-                            <li class="header-menu-item menu-item list-group-item overflow-auto my-2 mx-2">
+                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto my-2 mx-2">
                                 @include('components.logout-button', ['logout_btn' => '<i class="fa-solid fa-power-off"></i>', 'link_classes' => ''])
                             </li>
                         @endauth
