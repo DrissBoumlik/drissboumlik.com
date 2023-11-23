d5cddefaad483929574394bd230877c74efdeb32
Driss <drissboumlik.dev@gmail.com>
2023-11-23 23:56:56 +0100

    Edit login page layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,49 +3,9 @@
 @section('content')
     <div class="container-fluid auth-box">
         <section class="vh-100">
-            <div class="container-fluid d-flex justify-content-center align-items-center h-100">
+            <div class="container d-flex justify-content-center align-items-center h-100">
                 <div class="row d-flex justify-content-center align-items-center w-100">
                     <div class="col-lg-6 col-md-10 login-block">
-                        @if (env('APP_ENV') == 'local')
-                        <form class="login-form" method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <!-- Email input -->
-                            <div class="form-outline mb-2">
-                                <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" placeholder="Enter a valid email address"
-                                name="email" value="{{ 'admin@admin.com' ?? old('email')}}" required autocomplete="email" autofocus/>
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <!-- Password input -->
-                            <div class="form-outline mb-2 password-form-input">
-                                <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter password"
-                                name="password" required autocomplete="current-password" value="password"/>
-                                <div class="show-password"><i class="fa-solid fa-eye"></i></div>
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="d-flex justify-content-between align-items-center mb-2">
-                                <!-- Checkbox -->
-                                <div class="form-check">
-                                    <input class="form-check-input me-2" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} />
-                                    <label class="form-check-label" for="remember">Remember me</label>
-                                </div>
-                                <div class="password-request">
-                                    <a href="{{ route('password.request') }}" class="">Forgot password?</a>
-                                </div>
-                            </div>
-                            <div class="form-outline mb-0">
-                                <button type="submit" class="btn btn-primary" id="log">Login</button>
-{{--                                <script>document.getElementById("log").click();</script>--}}
-                            </div>
-                        </form>
-                        @else
                         <form class="login-form" method="POST" action="{{ route('login') }}">
                             @csrf
                             <!-- Email input -->
@@ -82,8 +42,7 @@
                             <div class="form-outline mb-0">
                                 <button type="submit" class="btn btn-primary" id="log">Login</button>
                             </div>
-                </form>
-                        @endif
+                        </form>
                     </div>
                     <div class="col-lg-6 col-md-2 img-block">
                         <img src="{{ asset('/assets/img/activities/workshops.svg') }}" alt="" class="auth-img img-fluid w-100" width="300" height="300" loading="lazy">
