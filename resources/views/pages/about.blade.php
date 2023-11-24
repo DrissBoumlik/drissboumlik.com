fcf4866983bfa9463f5551e2de4d065e15f46304
Driss <drissboumlik.dev@gmail.com>
2023-11-25 00:10:13 +0100

    Edit about page layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -13,7 +13,7 @@
                 <div class="about-page about">
                     <div class="container">
                         <div class="row align-items-center justify-content-center">
-                            <div class="col-md-6 col-12 about-me-txt">
+                            <div class="col-lg-6 col-md-7 col-12 about-me-txt">
                                 <p>I'm <span class="fw-bold tc-blue">Driss Boumlik</span>, from Morocco, I code, blog, speak, teach, mentor ...
                                     and other stuff usually not in the same order.</p>
                                 <p>I like to build & share things that add value or make positive impact.</p>
@@ -24,7 +24,7 @@
                                     <a target="_blank" href="https://www.goodreads.com/review/list/170245827-driss?view=covers" rel="noopener">books</a>.</p>
                                 <p>Checkout my <a href="/resume">resume</a> and reach me through : <a href="mailto:hi@drissboumlik.com">hi@drissboumlik.com</a></p>
                             </div>
-                            <div class="col-md-6 col-10 col-11">
+                            <div class="col-lg-6 col-md-5 col-11">
                                 <div class="img-wrapper">
                                     <img class="img-fluid w-100" src="{{ asset('/assets/img/activities/coding-challenges.svg') }}" alt="" width="300" height="300" loading="lazy">
                                 </div>
