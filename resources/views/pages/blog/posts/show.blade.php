cda709a345c2e0312729db62d11cb9d33c41604a
Driss <drissboumlik.dev@gmail.com>
2023-11-24 13:31:44 +0100

    Edit post page layaout and style

--- resources/views/pages/blog/posts/show.blade.php
+++ resources/views/pages/blog/posts/show.blade.php
@@ -6,49 +6,43 @@
 @endsection
 
 @section('headline')
-    <div class="row">
-        <div class="col-12 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
-            <div class="post-title mb-3">
-                <h1 class="header-txt">{{ $post->title }}</h1>
-            </div>
-            <div class="post-meta-data">
-                <div class="post-date mb-1">
-                    @if($post->published_at)
-                        <span title="{{ $post->published_at }}">Posted: {{ $post->published_at_short_format }} • {{ $post->read_duration }} min read</span>
-                    @endif
-                    @auth
-                        ·
-                        <a href="/admin/posts/edit/{{ $post->slug }}" target="_blank" class="text-secondary animated-underline">
-                            <i class="fa fa-fw fa-pencil"></i> Edit
-                        </a>
-                    @endauth
-                </div>
-                @if ($post->tags)
-                    <div class="post-tags">
-                        @foreach ($post->tags as $tag)
-                            <div class="post-tag d-inline-block me-2">
-                                <i class="fa-solid fa-tag fs-small"></i>
-                                <a href="/tags/{{ $tag->slug }}">
-                                    <span>{{ $tag->name }}</span>
-                                </a>
-                            </div>
-                        @endforeach
-                    </div>
-                @endif
-            </div>
-        </div>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h1 class="header-txt post-title">{!! $post->title !!}</h1>
     </div>
 @endsection
 
 @section('page-content')
     <div class="container-fluid p-0">
-{{--        @include('pages.partials.about')--}}
         <div class="post-page">
             <div class="section">
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2 mb-4 post">
-
+                            <div class="post-meta-data d-flex flex-column align-items-center">
+                                <div class="post-date mb-1">
+                                    @if($post->published_at)
+                                        <span title="{{ $post->published_at }}">Posted: {{ $post->published_at_short_format }} • {{ $post->read_duration }} min read</span>
+                                    @endif
+                                    @auth
+                                        ·
+                                        <a href="/admin/posts/edit/{{ $post->slug }}" target="_blank" class="text-secondary animated-underline">
+                                            <i class="fa fa-fw fa-pencil"></i> Edit
+                                        </a>
+                                    @endauth
+                                </div>
+                                @if ($post->tags)
+                                    <div class="post-tags">
+                                        @foreach ($post->tags as $tag)
+                                            <div class="post-tag d-inline-block">
+                                                <i class="fa-solid fa-tag fs-small"></i>
+                                                <a href="/tags/{{ $tag->slug }}">
+                                                    <span>{{ $tag->name }}</span>
+                                                </a>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                @endif
+                            </div>
                             <div class="post-content mt-3">
                                 {!! $post->content !!}
                             </div>
