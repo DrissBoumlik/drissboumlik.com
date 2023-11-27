f12d98298588f10273b78369decb3a5b3d2739bc
Driss <drissboumlik.dev@gmail.com>
2023-11-27 15:41:19 +0100

    Add logo and edit menu items spacings

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -1,12 +1,19 @@
 <div class="menu nav-scroller py-2">
     <div class="container">
         <div class="menu-blocks d-flex justify-content-center align-items-center">
+            <div class="brand turn-trigger me-2 d-none d-sm-block">
+                <a href="/" class="d-flex align-items-center">
+                    <div class="logo logo-brand turn position-relative d-inline-block">
+                        @include('components.logo-svg')
+                    </div>
+                </a>
+            </div>
             <nav class="nav d-flex justify-content-center">
                 <div class="header-menu-wrapper menu-wrapper">
                     <ul class="header-menu list-group list-group-horizontal">
                         @foreach ($headerMenu as $link)
                             <li class="header-menu-item menu-item list-group-item animated-underline
-                                        overflow-auto my-2 mx-2 {{ request()->is($link->slug) ? 'active' : '' }}">
+                                        overflow-auto {{ request()->is($link->slug) ? 'active' : '' }}">
                                 <a href="{{ \URL::to($link->slug) }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
                                    aria-label="{{ $link->title }}" class="text-capitalize">
                                     {!! $link->title !!}
@@ -14,13 +21,13 @@
                             </li>
                         @endforeach
                         @auth
-                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto my-2 mx-2">
+                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto">
                                 <a href="/admin" rel="noopener" target="_blank"
                                    aria-label="Admin Panel">
                                     <i class="fa-solid fa-gear"></i>
                                 </a>
                             </li>
-                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto my-2 mx-2">
+                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto">
                                 @include('components.logout-button', ['logout_btn' => '<i class="fa-solid fa-power-off"></i>', 'link_classes' => ''])
                             </li>
                         @endauth
