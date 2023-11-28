8e0a5a3a4ac2f53d7bc42017ed85f8cc194914ef
Driss <drissboumlik.dev@gmail.com>
2023-11-29 00:15:50 +0100

    Add action trigger to display search form

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -294,6 +294,28 @@ function initEvents() {
     $('.banner-close').on('click', function () {
         $(this).closest('.banner').remove();
     });
+
+    let search_blog_form_wrapper = $('.search-blog-form-wrapper');
+    if (search_blog_form_wrapper.length) {
+        $(window).keydown(function (event) {
+            if (event.ctrlKey && (event.key === 'k' || event.key === 'K')) {
+                event.preventDefault();
+                search_blog_form_wrapper.removeClass('d-none').addClass('show');
+                $('.search-blog-input').focus();
+            }
+        });
+        search_blog_form_wrapper.on('click', function (event) {
+            if (event.target === this) {
+                search_blog_form_wrapper.addClass('d-none');
+            }
+        });
+
+        $('.display-search-form').on('click', function() {
+            search_blog_form_wrapper.removeClass('d-none').addClass('show');
+            $('.search-blog-input').focus();
+        });
+    }
+
 }
 
 function shortenTextIfLongByLength(text, length, end = '...'){
