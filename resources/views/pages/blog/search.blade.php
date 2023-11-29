e3f58d89662418d18587b7e745d151bad5c0f9e7
Driss <drissboumlik.dev@gmail.com>
2023-11-30 00:52:22 +0100

    Remove search button form blog pages title

--- resources/views/pages/blog/search.blade.php
+++ resources/views/pages/blog/search.blade.php
@@ -2,7 +2,7 @@
 
 @section('headline')
     <div class="d-flex flex-column align-items-center justify-content-center">
-        <h1 class="header-txt">{!! $data->headline !!} <span class="display-search-form px-2"><i class="fa-solid fa-magnifying-glass"></i></span></h1>
+        <h1 class="header-txt fs-small text-transform-unset">{!! $data->headline !!}</h1>
     </div>
 @endsection
 
