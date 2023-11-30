228fe8ed48d791e5801161ea9e06a13c2f9bdc79
Driss <drissboumlik.dev@gmail.com>
2023-11-30 01:15:22 +0100

    Remove overflow auto

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -5,8 +5,7 @@
                 <div class="header-menu-wrapper menu-wrapper">
                     <ul class="header-menu list-group list-group-horizontal">
                         @foreach ($headerMenu as $link)
-                            <li class="header-menu-item menu-item list-group-item animated-underline
-                                        overflow-auto {{ request()->is($link->slug) ? 'active' : '' }}">
+                            <li class="header-menu-item menu-item list-group-item animated-underline {{ request()->is($link->slug) ? 'active' : '' }}">
                                 <a href="{{ \URL::to($link->slug) }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
                                    aria-label="{{ $link->title }}" class="text-capitalize">
                                     {!! $link->title !!}
@@ -14,7 +13,7 @@
                             </li>
                         @endforeach
                         @if(request()->is(['blog', 'tags', 'search']))
-                            <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto">
+                            <li class="header-menu-item menu-item list-group-item animated-underline">
                                 <span class="display-search-form"><i class="fa-solid fa-magnifying-glass"></i></span>
                             </li>
                         @endif
@@ -24,13 +23,13 @@
             @auth
             <div class="header-menu-wrapper login-menu-items flex-grow-2">
                 <ul class="header-menu list-group list-group-horizontal">
-                    <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto">
+                    <li class="header-menu-item menu-item list-group-item animated-underline">
                         <a href="/admin" rel="noopener" target="_blank"
                            aria-label="Admin Panel">
                             <i class="fa-solid fa-gear"></i>
                         </a>
                     </li>
-                    <li class="header-menu-item menu-item list-group-item animated-underline overflow-auto">
+                    <li class="header-menu-item menu-item list-group-item animated-underline">
                         @include('components.logout-button', ['logout_btn' => '<i class="fa-solid fa-power-off"></i>', 'link_classes' => ''])
                     </li>
                 </ul>
