6852d56ddcdfb28e373266adc8c6708811536051
Driss <drissboumlik.dev@gmail.com>
2023-12-01 15:01:26 +0100

    Edit resume menu links

--- resources/views/pages/resume/menu.blade.php
+++ resources/views/pages/resume/menu.blade.php
@@ -1,31 +1,31 @@
 <div class="resume-menu sections-menu">
     <ul class="sections-menu-items list-group">
         <li class="list-group-item" title="Experiences" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#experiences"><i class="fa-solid fa-layer-group"></i></a>
+            <a href="#experiences"><i class="fa-solid fa-layer-group"></i></a>
         </li>
         <li class="list-group-item" title="Competences" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#competences"><i class="fa-solid fa-gears"></i></a>
+            <a href="#competences"><i class="fa-solid fa-gears"></i></a>
         </li>
         <li class="list-group-item" title="Languages" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#languages"><i class="fa-solid fa-language"></i></a>
+            <a href="#languages"><i class="fa-solid fa-language"></i></a>
         </li>
         <li class="list-group-item" title="Additional Skills" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#add-skills"><i class="fa-solid fa-circle-plus"></i></a>
+            <a href="#add-skills"><i class="fa-solid fa-circle-plus"></i></a>
         </li>
         <li class="list-group-item" title="Education" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#education"><i class="fa-solid fa-user-graduate"></i></a>
+            <a href="#education"><i class="fa-solid fa-user-graduate"></i></a>
         </li>
         <li class="list-group-item" title="Work" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#work"><i class="fa-solid fa-briefcase"></i></a>
+            <a href="#work"><i class="fa-solid fa-briefcase"></i></a>
         </li>
         <li class="list-group-item" title="Passion" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#passion"><i class="fa-solid fa-plug-circle-check"></i></a>
+            <a href="#passion"><i class="fa-solid fa-plug-circle-check"></i></a>
         </li>
         <li class="list-group-item" title="Other Experiences" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#other-experiences"><i class="fa-solid fa-person-digging"></i></a>
+            <a href="#other-experiences"><i class="fa-solid fa-person-digging"></i></a>
         </li>
         <li class="list-group-item" title="Testimonials" data-toggle="tooltip" data-placement="left">
-            <a href="/resume#testimonials"><i class="fa-solid fa-comments"></i></a>
+            <a href="#testimonials"><i class="fa-solid fa-comments"></i></a>
         </li>
     </ul>
 </div>
