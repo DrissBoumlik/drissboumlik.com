0c619353c08339f611b0ea0c0fa7cf8e455fde9a
Driss <drissboumlik.dev@gmail.com>
2023-12-03 16:46:10 +0100

    Clean vite config

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,13 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/sass/app.sass',
                 'resources/sass/externals.sass',
                 'resources/template/sass/main.scss',
-                'resources/template/sass/oneui/themes/amethyst.scss',
-                'resources/template/sass/oneui/themes/city.scss',
-                'resources/template/sass/oneui/themes/flat.scss',
-                'resources/template/sass/oneui/themes/modern.scss',
-                'resources/template/sass/oneui/themes/smooth.scss',
                 'resources/js/admin/app.js',
                 'resources/js/app.js',
                 'resources/js/pages/code-animation.js',
                 'resources/js/pages/contact.js',
-                'resources/template/js/oneui/app.js',
-                'resources/template/js/pages/datatables.js',
-                'resources/template/js/pages/slick.js',
             ],
             refresh: true,
         }),
