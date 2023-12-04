1471022f9e7ec6f2f78c4a59240a5398b44ad57e
Driss <drissboumlik.dev@gmail.com>
2023-12-04 07:30:26 +0100

    Edit admin footer dom order

--- resources/views/admin/template/backend-sections/footer.blade.php
+++ resources/views/admin/template/backend-sections/footer.blade.php
@@ -2,11 +2,11 @@
 <footer id="page-footer" class="bg-body-light">
     <div class="content py-3">
         <div class="row fs-sm">
-            <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://drissboumlik.com" target="_blank">DB</a>
+            <div class="col-sm-6 py-1 text-center text-sm-start">
+                <a class="fw-semibold" href="/admin" target="_blank">Admin Panel</a> &copy; {{ now()->year }}
             </div>
-            <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
-                <a class="fw-semibold" href="/" target="_blank">Admin Panel</a> &copy; <span data-toggle="year-copy"></span>
+            <div class="col-sm-6 py-1 text-center text-sm-end">
+                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="/" target="_blank">DB</a>
             </div>
         </div>
     </div>
