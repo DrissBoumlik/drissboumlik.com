8792fd3496c4e40727d46a5c2095a44793036a8d
Driss <drissboumlik.dev@gmail.com>
2023-12-06 15:48:44 +0100

    Restore fonts assets refs

--- resources/views/layout/header-parts/assets.blade.php
+++ resources/views/layout/header-parts/assets.blade.php
@@ -1,4 +1,10 @@
 
+<!-- Fonts -->
+<link rel="preload" href="{{ asset('/webfonts/fa-solid-900.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset('/webfonts/fa-brands-400.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset('/webfonts/fa-regular-400.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset('/assets/fonts/istokweb.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset('/assets/fonts/UntitledSansWeb-Bold.woff2') }}" as="font" type="font/woff2" crossorigin>
 
 @vite(['resources/sass/externals.sass'])
 @vite(['resources/sass/app.sass'])
