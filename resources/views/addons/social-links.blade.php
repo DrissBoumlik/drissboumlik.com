c8ee62cc144e911ccf87bc3e74b76f07c61b9701
Driss <drissboumlik.dev@gmail.com>
2023-12-11 19:53:56 +0100

    Add tooltip for social links

--- resources/views/addons/social-links.blade.php
+++ resources/views/addons/social-links.blade.php
@@ -4,7 +4,8 @@
             <li class="list-group-item border-0 py-0 px-2">
                 <a href="{{ $socialLink->link }}" target="{{ $socialLink->target ?? '_blank' }}" rel="noopener"
                     aria-label="{{ $socialLink->title }}" title="{{ $socialLink->title }}"
-                    class="text-decoration-none">
+                   data-toggle="tooltip" data-placement="left"
+                   class="text-decoration-none">
                     <span class="social-icon">{!! $socialLink->icon !!}</span>
                 </a>
             </li>
