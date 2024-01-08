5aec30b72441f1057bef797832efd9dd7cf1a415
Driss <drissboumlik.dev@gmail.com>
2024-01-08 21:01:05 +0100

    Remove unused files

--- resources/template/js/pages/slick.js
+++
@@ -1,8 +0,0 @@
-/*
- *  Document   : slick.js
- *  Author     : pixelcave
- *  Description: Using a JS helper to init Slick Slider plugin
- */
-
-// Slick Slider, for more info and examples you can check out http://kenwheeler.github.io/slick/
-One.helpersOnLoad(['jq-slick']);
