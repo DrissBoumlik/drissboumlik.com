6a8ec35b63b830e7ef545216540b2161e67d1543
Driss <drissboumlik.dev@gmail.com>
2024-01-22 01:20:35 +0100

    Edit headline subscription pages & fix token value on update

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -9,19 +9,19 @@ class SubscriberController extends Controller
 {
     public function show(Request $request, $uuid)
     {
-        $data = pageSetup('Subscription | Update Data', 'Subscription', true, true);
+        $data = pageSetup('Subscription | Update Data', 'update subscription data', true, true);
         $subscriber = Subscriber::where('subscription_id', $uuid)->first();
         return view('pages.subscription.verify', ['data' => $data, 'subscriber' => $subscriber]);
     }
 
     public function verifySubscribtion(Request $request, $token)
     {
-        $data = pageSetup('Subscription | Update Data', null, true, true);
+        $data = pageSetup('Subscription | Update Data', 'subscription verification', true, true);
         $subscriber = Subscriber::where('token_verification', $token)->first();
         if ($subscriber) {
             $subscriber->update([
                 "subscribed_at" => now(),
-//                "token_verification" => null,
+                "token_verification" => null,
                 "subscription_id" => \Str::uuid(),
             ]);
         }
