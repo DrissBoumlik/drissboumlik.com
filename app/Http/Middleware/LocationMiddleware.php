198f7efd17e760ff6a1ac67589b5590f2c23287a
Driss <drissboumlik.dev@gmail.com>
2024-02-09 00:43:05 +0100

    Send email to admin in case of location issue (wip)

--- app/Http/Middleware/LocationMiddleware.php
+++ app/Http/Middleware/LocationMiddleware.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Visitor;
 use Closure;
 use Illuminate\Http\Request;
-use Stevebauman\Location\Facades\Location;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
 
 class LocationMiddleware
 {
@@ -18,25 +19,35 @@ class LocationMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $ip = $request->ip();
-//        $ip = '105.158.165.210';
-//        $ip = '48.188.144.248';
-        $currentUserInfo = Location::get($ip);
-        if ($currentUserInfo) {
-            $currentUserInfo->ref_source = $request->get('ref_src');
-            $currentUserInfo->ref_medium = $request->get('ref_mdm');
-            $route = $request->decodedPath();
-            $currentUserInfo->url = $route === '/' ? 'Home' : "/$route";
-            $visitor = Visitor::where('ip', $ip)->orderBy('updated_at', 'desc')->first();
-            if ($visitor) {
-                $timeSinceLastVisit = now()->diffInRealSeconds($visitor->updated_at);
-                $timeSinceLastVisitMinValue = 7200;
-                if ($timeSinceLastVisit > $timeSinceLastVisitMinValue) {
-                    Visitor::create((array)$currentUserInfo);
-                }
-            } else {
-                Visitor::create((array)$currentUserInfo);
+        $route = $request->decodedPath();
+        $req_data = [
+            'ip' => $request->ip(),
+            'client_name' => env('APP_NAME'),
+            'ref_src' => 'facebook', //$request->get('ref_src'),
+            'ref_mdm' => 'banner', //$request->get('ref_mdm'),
+            'visited_url' => $route === '/' ? 'Home' : "/$route",
+        ];
+       
+
+        $url_ws = env('APP_WS_URL') . '/api/visitors';
+        $response = Http::post($url_ws, $req_data);
+        if ($response->failed()) {
+            $response_data = $response->json();
+            $content_data = "<ul>";
+            foreach($response_data as $key => $response_item) {
+                $content_data .= "<li>$key : $response_item</li>\n";
             }
+            $content_data .= "</ul>";
+            $htmlContent = "<html><body>
+                            <h3>An issue occured with visitors service!</h1>
+                            <p>Here is the returned response:</p>
+                            <p>$content_data</p>
+                            </body></html>";
+            Mail::raw($htmlContent, function(Message $message) use ($htmlContent) {
+                $message->to(env('MAIL_FROM_ADDRESS'))
+                        ->subject('An issue occured with visitors service')
+                        ->html($htmlContent, 'text/html');
+            });
         }
         return $next($request);
     }
