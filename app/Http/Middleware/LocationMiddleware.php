13eae6b399b43129130c8c3a6298dd61251d7b11
Driss <drissboumlik.dev@gmail.com>
2024-02-09 16:14:43 +0100

    Log internally in case of email issue

--- app/Http/Middleware/LocationMiddleware.php
+++ app/Http/Middleware/LocationMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Mail\Message;
 
@@ -27,27 +28,43 @@ class LocationMiddleware
             'ref_mdm' => 'banner', //$request->get('ref_mdm'),
             'visited_url' => $route === '/' ? 'Home' : "/$route",
         ];
-       
+
 
         $url_ws = env('APP_WS_URL') . '/api/visitors';
         $response = Http::post($url_ws, $req_data);
         if ($response->failed()) {
-            $response_data = $response->json();
-            $content_data = "<ul>";
-            foreach($response_data as $key => $response_item) {
-                $content_data .= "<li>$key : $response_item</li>\n";
-            }
-            $content_data .= "</ul>";
-            $htmlContent = "<html><body>
-                            <h3>An issue occured with visitors service!</h1>
+            try {
+                $response_data = $response->json();
+                if (! $response_data || ! is_array($response_data)) {
+                    throw new \Exception("Something went wrong with the WS!");
+                }
+                $content_data = "<ul>";
+                foreach ($response_data as $key => $response_item) {
+                    $content_data .= "<li>$key : $response_item</li>\n";
+                }
+                $content_data .= "</ul>";
+                $htmlContent = "<html><body>
+                            <h3>An issue occurred with visitors service!</h1>
                             <p>Here is the returned response:</p>
                             <p>$content_data</p>
                             </body></html>";
-            Mail::raw($htmlContent, function(Message $message) use ($htmlContent) {
-                $message->to(env('MAIL_FROM_ADDRESS'))
-                        ->subject('An issue occured with visitors service')
-                        ->html($htmlContent, 'text/html');
-            });
+                Mail::raw($htmlContent, function(Message $message) use ($htmlContent) {
+                    $message->to(env('MAIL_FROM_ADDRESS'))
+                            ->subject('An issue occurred with visitors service')
+                            ->html($htmlContent, 'text/html');
+                });
+                $message = "\n==============================================================\n";
+                $message .= "An issue occurred with visitors service or location middleware\n";
+                $message .= "Checkout the email sent to the admin, this app's or webservice internal log\n";
+                $message .= "==============================================================\n\n";
+                Log::info($message);
+            } catch (\Throwable $e) {
+                $message = "\n==============================================================\n";
+                $message .= "An issue occurred with visitors service or location middleware\n";
+                $message .= $e->getMessage();
+                $message .= "\n==============================================================\n\n";
+                Log::error($message);
+            }
         }
         return $next($request);
     }
