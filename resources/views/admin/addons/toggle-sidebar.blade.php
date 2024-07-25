672e9df07301df5466ea4d54ad50b0bdb8b2132d
Driss <drissboumlik.dev@gmail.com>
2024-07-25 13:27:50 +0100

    Refactor toggle sidebar block

---
+++ resources/views/admin/addons/toggle-sidebar.blade.php
@@ -0,0 +1,13 @@
+<!-- Toggle Sidebar -->
+<!-- Layout API, functionality initialized in Template._uiApiLayout()-->
+<button type="button" class="btn btn-sm btn-alt-secondary d-lg-none" data-toggle="layout" data-action="sidebar_toggle">
+    <i class="fa fa-fw fa-bars"></i>
+</button>
+<!-- END Toggle Sidebar -->
+
+<!-- Toggle Mini Sidebar -->
+<!-- Layout API, functionality initialized in Template._uiApiLayout()-->
+<button type="button" class="btn btn-sm btn-alt-secondary d-none d-lg-inline-block" data-toggle="layout" data-action="sidebar_mini_toggle">
+    <i class="fa fa-fw fa-ellipsis-v"></i>
+</button>
+<!-- END Toggle Mini Sidebar -->
