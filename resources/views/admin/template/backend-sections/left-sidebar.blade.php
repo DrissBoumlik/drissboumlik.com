12fd114f82eeafa3283ba319121aa91904114a30
Driss <drissboumlik.dev@gmail.com>
2024-09-01 15:32:59 +0100

    Handle active links for sub pages

--- resources/views/admin/template/backend-sections/left-sidebar.blade.php
+++ resources/views/admin/template/backend-sections/left-sidebar.blade.php
@@ -61,7 +61,7 @@
         <!-- Side Navigation -->
         <div class="content-side">
             <ul class="nav-main">
-                <li class="nav-main-item{{ request()->is(['admin/posts', 'admin/tags', 'admin/media-manager']) ? ' open' : '' }}">
+                <li class="nav-main-item{{ request()->is(['admin/posts/*', 'admin/tags/*', 'admin/media-manager/*']) ? ' open' : '' }}">
                     <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
                         <i class="nav-main-link-icon fa fa-fw fa-cube"></i>
                         <span class="nav-main-link-name">Content Management</span>
