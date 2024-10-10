72b94ffaf9b2b5ec5e929bca9eca48276d6c9916
Driss <drissboumlik.dev@gmail.com>
2024-10-10 12:53:31 +0100

    Unload unused fonts

--- resources/views/layout/header-parts/assets.blade.php
+++ resources/views/layout/header-parts/assets.blade.php
@@ -11,16 +11,16 @@
       crossorigin>
 <link rel="preload" href="{{ Vite::asset('resources/assets/fonts/UntitledSansWeb-Bold.woff2') }}" as="font"
       type="font/woff2" crossorigin>
-<link rel="preload" href="{{ Vite::asset('resources/assets/fonts/Harmattan-Regular.ttf') }}" as="font" type="font/ttf"
-      crossorigin>
+{{--<link rel="preload" href="{{ Vite::asset('resources/assets/fonts/Harmattan-Regular.ttf') }}" as="font" type="font/ttf"--}}
+{{--      crossorigin>--}}
 <link rel="preload" href="{{ Vite::asset('resources/assets/fonts/My Epic Selfie.woff2') }}" as="font" type="font/woff2"
       crossorigin>
-<link rel="preload" href="{{ Vite::asset('resources/assets/fonts/Helvetica.ttf') }}" as="font" type="font/ttf"
-      crossorigin>
+{{--<link rel="preload" href="{{ Vite::asset('resources/assets/fonts/Helvetica.ttf') }}" as="font" type="font/ttf"--}}
+{{--      crossorigin>--}}
 <link rel="preload" href="{{ Vite::asset('resources/assets/fonts/Fira_Code_v6.2/woff2/FiraCode-Regular.woff2') }}"
       as="font" type="font/woff2" crossorigin>
-<link rel="preload" href="{{ Vite::asset('resources/assets/fonts/font-mfizz.woff') }}" as="font" type="font/woff"
-      crossorigin>
+{{--<link rel="preload" href="{{ Vite::asset('resources/assets/fonts/font-mfizz.woff') }}" as="font" type="font/woff"--}}
+{{--      crossorigin>--}}
 
 @vite(['resources/sass/externals.sass'])
 @vite(['resources/sass/app.sass'])
