39dd638caa772df78510edaa780eb2c8b1a3161a
Driss <drissboumlik.dev@gmail.com>
2024-10-11 10:51:54 +0100

    Add async to loaded scripts

--- resources/views/layout/header-parts/assets.blade.php
+++ resources/views/layout/header-parts/assets.blade.php
@@ -24,5 +24,5 @@
 
 @vite(['resources/sass/externals.sass'])
 @vite(['resources/sass/app.sass'])
-<script src="{{ asset('/plugins/particles/particles.min.js') }}"></script>
+<script async src="{{ asset('/plugins/particles/particles.min.js') }}"></script>
 @vite(['resources/js/app.js'])
