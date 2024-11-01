8b8be729cabf9474c78db60b69f6fb3d2e0200d2
Driss <drissboumlik.dev@gmail.com>
2024-11-01 12:01:08 +0100

    Fix alert dismiss button

--- resources/js/shared/functions.js
+++ resources/js/shared/functions.js
@@ -52,27 +52,23 @@ function get_alert_box(params, removeLoader = true) {
     }
 
     let alertElementHTML = `
-        <div data-notify="container" class="col-11 col-sm-8 col-md-5 col-lg-4 alert ${params.class} alert-dismissible animated fadeIn" role="alert" data-notify-position="bottom-right"
+        <div data-notify="container" class="col-11 col-sm-8 col-md-5 col-lg-4 alert ${params.class}
+            alert-dismissible animated fadeIn" role="alert" data-notify-position="bottom-right"
             style="display: inline-block; margin: 0px auto; position: fixed; transition: all 0.5s ease-in-out 0s; z-index: 1060; bottom: 20px; right: 20px; animation-iteration-count: 1;">
             <p class="mb-0">
                 <span data-notify="icon">${params.icon}</span>
                 <span data-notify="title"></span>
                 <span data-notify="message">${params.message}</span>
             </p>
-            <a class="p-2 m-1 text-dark" href="javascript:void(0)" aria-label="Close" data-notify="dismiss" style="position: absolute; right: 10px; top: 5px; z-index: 1035;">
+            <a class="p-2 m-1 text-dark" href="javascript:void(0)" aria-label="Close" data-notify="dismiss"
+                data-bs-dismiss="alert"
+                style="position: absolute; right: 10px; top: 5px; z-index: 1035;">
                 <i class="fa fa-times"></i>
             </a>
         </div>
     `;
 
     document.body.insertAdjacentHTML('beforeend', alertElementHTML);
-
-    let dismissibleAlert = document.querySelector('.alert.alert-dismissible');
-    if (dismissibleAlert) {
-        dismissibleAlert.addEventListener('click', function() {
-            dismissibleAlert.remove();
-        });
-    }
 
 }
 
