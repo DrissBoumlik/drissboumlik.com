339528dcc5a23258144b49a982ea9095cdcb6170
Driss <drissboumlik.dev@gmail.com>
2024-12-21 13:39:43 +0100

    Remove useless code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,11 @@
-import { drawText, initParticlesJS, initDarkMode } from "./functions";
+import { initParticlesJS, initDarkMode } from "@/functions";
 import 'lazysizes';
 
 document.addEventListener('DOMContentLoaded', function () {
     try {
-        drawText();
         initParticlesJS();
         initDarkMode();
     } catch (error) {
-        // console.log(error);
+        console.log(error);
     }
 });
