4a77eeacb2eccdd06f952d250399941b595ee559
Driss <drissboumlik.dev@gmail.com>
2024-12-30 14:39:45 +0100

    Move post editor init function to helpers

--- resources/js/admin/pages/post.js
+++ resources/js/admin/pages/post.js
@@ -1,5 +1,5 @@
 import { initFlatpickr, initSelect2, setUpImagePreviewOnFileInput } from "../../shared/helpers";
-import {getCookie, get_alert_box} from "@/shared/functions";
+import {getCookie, get_alert_box, initPostEditor} from "@/shared/functions";
 import {string_to_slug} from "@/admin/functions";
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -113,54 +113,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-function initPostEditor() {
-    if (! document.getElementById('post_body')) return;
-    let options = {
-        selector: 'textarea#post_body',
-        plugins: `searchreplace autolink visualblocks visualchars media charmap nonbreaking anchor insertdatetime
-                lists advlist wordcount help emoticons autosave code link table codesample image preview pagebreak
-                accordion`,
-        toolbar: `code codesample emoticons link image pagebreak | undo redo restoredraft | bold italic underline
-                | alignleft aligncenter alignright alignjustify lineheight indent outdent | bullist numlist
-                | accordion visualblocks visualchars searchreplace`,
-        pagebreak_separator: '<hr/>',
-        height: 700,
-        fixed_toolbar_container: '.tox-editor-header',
-        codesample_languages: [
-            { text: 'Bash', value: 'bash' },
-            { text: 'Typscript', value: 'typscript' },
-            { text: 'Markdown', value: 'markdown' },
-            { text: 'Pug', value: 'pug' },
-            { text: 'Sass', value: 'sass' },
-            { text: 'Yaml', value: 'yaml' },
-            { text: 'SQL', value: 'sql' },
-            { text: 'Powershell', value: 'powershell' },
-            { text: 'DOS', value: 'dos' },
-            { text: 'Batch', value: 'batch' },
-            { text: 'HTML/XML', value: 'markup' },
-            { text: 'CSS', value: 'css' },
-            { text: 'JavaScript', value: 'javascript' },
-            { text: 'PHP', value: 'php' },
-            { text: 'Ruby', value: 'ruby' },
-            { text: 'Python', value: 'python' },
-            { text: 'Java', value: 'java' },
-            { text: 'C', value: 'c' },
-            { text: 'C#', value: 'csharp' },
-            { text: 'C++', value: 'cpp' }
-        ],
-    };
-    let theme = getCookie('theme');
-    if (theme === 'dark-mode') {
-        options = {...options,  skin: 'oxide-dark', content_css: 'dark'};
-    }
-    let tinymceDOM = tinymce.get('post_body');
-    if(tinymceDOM != null) {
-        let _content = tinymceDOM.getContent();
-        tinymceDOM.destroy();
-        tinymceDOM.setContent(_content);
-    }
-    tinymce.init(options);
-}
 
 function fillPostAssetsModal(postAssets){
     let gallery = `<div class="col-12"><div class="text-center p-5">No assets found</div></div>`;
@@ -219,5 +171,3 @@ function fillPostAssetsModal(postAssets){
 
     modalPostAssets.style.display = 'block';
 }
-
-export { initPostEditor }
