072c7b4fdbe399c701b545378c0fda8711d6115d
Driss <drissboumlik.dev@gmail.com>
2025-01-22 14:05:39 +0100

    Restrict access to pulse dashboard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('viewPulse', function ($user) {
+            return Auth::check();
+        });
     }
 }
